f326f0a6c4a3a461711f0361271fc877ab755456
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-01-25 14:29:33 -0300

    Removendo arquivos

--- prisma/migrations/20250124181504_update_fields/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `fulname` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "fulname",
-ADD COLUMN     "fullname" TEXT;
