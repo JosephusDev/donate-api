dce3dbbbf8292d5eb43f43ddef0e8f8bcbb0ee2f
OliverOliveira <filomenoolivetree@gmail.com>
2025-02-03 18:50:33 +0100

    feat: get donates

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { create, login } from '../controllers/User'
+import { create, login, getDonates } from '../controllers/User'
 
 const router = Router()
 
 router.post('/', create)
 router.post('/login', login)
+router.get('/donates', getDonates)
 
 export default router
