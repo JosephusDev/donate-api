0ecba747d8ddccbbfb58a0139141b7b3ef610a84
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-05-11 02:00:12 -0300

    feat: Email and update profile

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
-import { create, login } from '../controllers/user'
+import { create, login, update } from '../controllers/user'
 
 const router = Router()
 
 router.post('/', create)
 router.post('/login', login)
+router.put('/update/:id', update)
 
 export default router
