4021ab8d66f37f03c2ff99e54365b607da0bc2d9
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-05-16 20:28:00 -0300

    modified:   src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import donateRoutes from './routes/donate.ts'
+import chatRoutes from './routes/chat.ts'
+import { authenticateToken } from './middleware/Authentication.ts'
 import userRoutes from './routes/User.ts'
+import orderRoutes from './routes/Order.ts'
+import bloodTypeRoutes from './routes/blood_type.ts'
 
 const app = express()
 
@@ -8,5 +13,11 @@ app.use(express.json())
 app.use(cors())
 //Public routes
 app.use('/user', userRoutes)
+
+//Private routes
+app.use('/order', authenticateToken, orderRoutes)
+app.use('/bloodtypes', authenticateToken, bloodTypeRoutes)
+app.use('/donates', authenticateToken, donateRoutes)
+app.use('/chat', authenticateToken, chatRoutes)
 
 export { app }
