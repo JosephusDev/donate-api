44e1f78d83395bd819f77376934bd5cb6b371aa8
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-05-16 20:18:22 -0300

    modified:   src/routes/User.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,6 @@
 import express from 'express'
 import cors from 'cors'
-import donateRoutes from './routes/donate.ts'
-import chatRoutes from './routes/chat.ts'
-import { authenticateToken } from './middleware/Authentication.ts'
 import userRoutes from './routes/User.ts'
-import orderRoutes from './routes/Order.ts'
-import bloodTypeRoutes from './routes/blood_type.ts'
 
 const app = express()
 
@@ -13,11 +8,5 @@ app.use(express.json())
 app.use(cors())
 //Public routes
 app.use('/user', userRoutes)
-
-//Private routes
-app.use('/order', authenticateToken, orderRoutes)
-app.use('/bloodtypes', authenticateToken, bloodTypeRoutes)
-app.use('/donates', authenticateToken, donateRoutes)
-app.use('/chat', authenticateToken, chatRoutes)
 
 export { app }
--- src/routes/User.ts
+++ src/routes/User.ts
@@ -1,8 +1,11 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 import { create, login, update } from '../controllers/User.ts'
 
 const router = Router()
 
+router.get('/', (req: Request, res: Response) => {
+	res.status(200).json({ message: 'Hello World' })
+})
 router.post('/', create)
 router.post('/login', login)
 router.put('/update/:id', update)
