59a5ffaf7b8c23438b835b5dd3c6a73f570efdfa
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-05-16 20:02:42 -0300

    modified:   src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,14 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
-import donateRoutes from './routes/donate.ts'
-import chatRoutes from './routes/chat.ts'
-import { authenticateToken } from './middleware/Authentication.ts'
-import userRoutes from './routes/User.ts'
-import orderRoutes from './routes/Order.ts'
-import bloodTypeRoutes from './routes/blood_type.ts'
 
 const app = express()
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-	console.error('Unhandled Error:', err)
-	res.status(500).json({ error: 'Internal Server Error' })
+app.use('/', (req: Request, res: Response) => {
+	console.log(`${req.method} ${req.url}`)
+	res.status(200).json({ message: 'Hello World' })
 })
 
 app.use(express.json())
 app.use(cors())
-//Public routes
-app.use('/user', userRoutes)
-
-//Private routes
-app.use('/order', authenticateToken, orderRoutes)
-app.use('/bloodtypes', authenticateToken, bloodTypeRoutes)
-app.use('/donates', authenticateToken, donateRoutes)
-app.use('/chat', authenticateToken, chatRoutes)
 
 export { app }
