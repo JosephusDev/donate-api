e4edb9b6ff007db276c130efa548f9376c62ad9a
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-05-17 00:18:44 -0300

    teste

--- dist/routes/blood_type.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const blood_type_1 = require("../controllers/blood_type");
-const router = (0, express_1.Router)();
-router.get('/', blood_type_1.getBloodType);
-exports.default = router;
