eeee7fa79cf5dfc43486c073d588758e1bba1d1f
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-05-16 21:23:08 -0300

    modified:   vercel.json

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
-import app from './app.ts'
+import express from 'express'
+import cors from 'cors'
+import donateRoutes from './routes/donate.ts'
+import chatRoutes from './routes/chat.ts'
+import { authenticateToken } from './middleware/Authentication.ts'
+import userRoutes from './routes/User.ts'
+import orderRoutes from './routes/Order.ts'
+import bloodTypeRoutes from './routes/blood_type.ts'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+//Public routes
+app.use('/user', userRoutes)
+
+//Private routes
+app.use('/order', authenticateToken, orderRoutes)
+app.use('/bloodtypes', authenticateToken, bloodTypeRoutes)
+app.use('/donates', authenticateToken, donateRoutes)
+app.use('/chat', authenticateToken, chatRoutes)
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000
 app.listen(PORT, () => {
